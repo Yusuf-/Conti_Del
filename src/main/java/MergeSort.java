085a6108aef03ef2d3993d92f1bfa6696dbe0dec
yusuf- <yusuf_a@live.nl>
2015-01-05 16:37:37 +0100

    5 Dec 16.37

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -1,9 +1,15 @@
 /**
  * Created by Yusuf on 6-12-2014.
+ * Merge sort is an algorithm that uses divide-and-conquer strategy to sort a sequence.
  */
-
 public class MergeSort {
 
+    /**
+     * Returns an array of selected size, that contains random values.
+     *
+     * @param size an integer that sets the size of your array.
+     * @return random array
+     */
     public int[] randomArray(int size){
         int[] array = new int[size];
         for(int i = 0; i < array.length; i++){
@@ -13,6 +19,12 @@ public class MergeSort {
         return array;
     }
 
+    /**
+     * Sorts the array by splitting until there are only base cases. Then merges the base cases
+     * @param array the unsorted array that needs to be sorted
+     * @param p integer that gives the begin position of the array
+     * @param r integer that gives the length of the array
+     */
     public void mergeSort(int[] array, int p, int r){
         if(p < r){
             int q = (p + r) / 2;
@@ -23,6 +35,13 @@ public class MergeSort {
         }
     }
 
+    /**
+     * Merges the base cases
+     * @param array the unsorted array that needs to be sorted
+     * @param p integer that gives the begin position of the array
+     * @param q integer that gives the middle position of the array. This calculated by (p + r) / 2
+     * @param r integer that gives the length of the array
+     */
     public void merge(int[] array, int p, int q, int r){
         int n1 = q- p +1;
         int n2 = r - q;
@@ -54,6 +73,11 @@ public class MergeSort {
         }
     }
 
+    /**
+     * Method that is used to check if the array is sorted
+     * @param array the array that will be checked
+     * @return True if the array is sorted. False if array is not sorted
+     */
     public boolean isSorted(int[] array){
         boolean sorted = true;
         for(int i = 0; i < array.length - 1; i++){
@@ -64,26 +88,17 @@ public class MergeSort {
         }
         return sorted;
     }
-    // New method
-    public void printNumber(){
-        System.out.println(5);
-    }
-    public void printArray(int[] array){
-        for(int k: array){
-            System.out.print(k + " ");
-        }
-    }
-
-    public void randomMethod(){
-
-    }
 
     public static  void  main(String[] args){
         MergeSort myMergeSort = new MergeSort();
         int[] myArray = myMergeSort.randomArray(6);
-        myMergeSort.mergeSort(myArray,0,myArray.length-1);
+        myMergeSort.mergeSort(myArray,0,myArray.length);
 
-        myMergeSort.printArray(myArray);
+        for(int k: myArray){
+            System.out.print(k + " ");
+        }
+
+        System.out.println(myMergeSort.isSorted(myArray));
     }
 }
 
